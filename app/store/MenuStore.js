1b583cfc83f0299a1425d3640d79a39f0aa959ec
Mahatma_Fatal_Error <mahatmafatal@gmail.com>
2012-03-08 16:12:29 +0100

    Changed the Menu List to NestedList
    with ale dependencies

--- app/store/MenuStore.js
+++ app/store/MenuStore.js
@@ -1,16 +1,16 @@
 Ext.define('MyApp.store.MenuStore', {
-    extend: 'Ext.data.Store',
+    extend: 'Ext.data.TreeStore',
 //    requires: 'MyApp.Model.Main',
     
     config: {
         model: 'MyApp.model.Menu',
         storeId: 'MenuStore',
         autoLoad: true,
-        //defaultRootProperty: 'items',   //default?
+        defaultRootProperty: 'items',   //default?
         
         proxy:{
             type: 'ajax',
-            url: 'data/menu.json',
+            url: 'data/menu_nested.json',
             
             reader: {
                 type: 'json'
